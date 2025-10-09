f09e82d56ec3ed36a4918229ba985d937609920a
Nicolas Arias <103001328+aarial12@users.noreply.github.com>
2025-10-09 15:59:31 -0500

    Delete ejercicio17 directory

--- ejercicio17/Circulo.py
+++
@@ -1,9 +0,0 @@
-import math
-
-class Circulo:
-    
-    def area(radio):
-        return math.pi * pow(radio,2) 
-    
-    def circunferencia(radio):
-        return math.pi * (2 * radio)
--- ejercicio17/main.py
+++
@@ -1,6 +0,0 @@
-from Circulo import *
-
-radio = float(input("Ingrese el radio del circulo: ")) 
-
-print("El area del circulo es:", Circulo.area(radio), "unidades cuadradas.")
-print("La longitud de la circunferencia es:", Circulo.circunferencia(radio), "unidades.")
