5db184321b3ea5d3b364730c7a2633b12e2c56f5
Nicolas Arias <103001328+aarial12@users.noreply.github.com>
2025-10-09 16:04:25 -0500

    Delete App.java

--- App.java
+++
@@ -1,9 +0,0 @@
-package ejercicio1;
-import ejercicio1.src.Notas.Ventana;
-
-public class App {
-    public static void main(String[] args) {
-        Ventana ventana = new Ventana();
-        ventana.setVisible(true);
-    }
-}
