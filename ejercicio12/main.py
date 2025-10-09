4906ea80e8b265398a372865a1962f5642e76554
Nicolas Arias <103001328+aarial12@users.noreply.github.com>
2025-10-09 15:59:12 -0500

    Delete ejercicio12 directory

--- ejercicio12/main.py
+++
@@ -1,7 +0,0 @@
-from Gobierno import Gobierno
-
-salarioBruto = Gobierno.salario_bruto()
-reteFuente = Gobierno.rete_fuente(salarioBruto)
-salairoNeto  = Gobierno.salario_neto(salarioBruto, reteFuente)
-
-print("Salario bruto:",salarioBruto,"\nRetencion en la fuente:",reteFuente,"\nSalario neto:",salairoNeto)
